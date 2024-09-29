4fb696131d6120a20f890e92cd16e79e61c8cc7d
David Friar <david.friar@cognifide.com>
2024-09-29 20:53:55 +0100

    initial commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,287 @@
+import localFont from "next/font/local"
+
+export const alegreya = localFont({
+  src: [
+    {
+      path: "./fonts/alegreya/Alegreya-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/alegreya/Alegreya-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/alegreya/Alegreya-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/alegreya/Alegreya-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-alegreya",
+})
+
+export const alegreyaSans = localFont({
+  src: [
+    {
+      path: "./fonts/alegreya-sans/AlegreyaSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/alegreya-sans/AlegreyaSans-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/alegreya-sans/AlegreyaSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/alegreya-sans/AlegreyaSans-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-alegreya-sans",
+})
+
+export const firaSans = localFont({
+  src: [
+    {
+      path: "./fonts/fira-sans/FiraSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/fira-sans/FiraSans-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/fira-sans/FiraSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/fira-sans/FiraSans-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-fira-sans",
+})
+
+export const bitter = localFont({
+  src: [
+    {
+      path: "./fonts/bitter/BitterPro-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/bitter/BitterPro-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/bitter/BitterPro-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/bitter/BitterPro-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-bitter",
+})
+
+export const lato = localFont({
+  src: [
+    {
+      path: "./fonts/lato/Lato-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/lato/Lato-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/lato/Lato-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/lato/Lato-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-lato",
+})
+
+export const urbanist = localFont({
+  src: "./fonts/urbanist/Urbanist[ital,wght].woff2",
+  variable: "--font-urbanist",
+})
+
+export const playfair = localFont({
+  src: [
+    {
+      path: "./fonts/playfair/PlayfairRomanVF.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/playfair/PlayfairItalicVF.woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-playfair",
+})
+
+export const sourceSerif = localFont({
+  src: [
+    {
+      path: "./fonts/source-serif/SourceSerif4Variable-Roman.otf.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/source-serif/SourceSerif4Variable-Italic.otf.woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-source-serif",
+})
+
+export const sourceSans = localFont({
+  src: [
+    {
+      path: "./fonts/source-sans/SourceSans3VF-Upright.otf.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/source-sans/SourceSans3VF-Italic.otf.woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-source-sans",
+})
+
+export const sourceCode = localFont({
+  src: [
+    {
+      path: "./fonts/source-code/SourceCodeVF-Upright.otf.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/source-code/SourceCodeVF-Italic.otf.woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-source-code",
+})
+
+export const merriweather = localFont({
+  src: [
+    {
+      path: "./fonts/merriweather/Merriweather[opsz,wdth,wght].woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/merriweather/Merriweather-Italic[opsz,wdth,wght].woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-merriweather",
+})
+
+export const merriweatherSans = localFont({
+  src: [
+    {
+      path: "./fonts/merriweather-sans/MerriweatherSans[wght].woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/merriweather-sans/MerriweatherSans-Italic[wght].woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-merriweather-sans",
+})
+
+export const inter = localFont({
+  src: [
+    {
+      path: "./fonts/inter/InterVariable.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/inter/InterVariable-Italic.woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-inter",
+})
+
+export const piazzolla = localFont({
+  src: [
+    {
+      path: "./fonts/piazzolla/Piazzolla[opsz,wght].woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/piazzolla/Piazzolla-Italic[opsz,wght].woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-piazzolla",
+})
+
+export const piazzollaSC = localFont({
+  src: [
+    {
+      path: "./fonts/piazzolla/PiazzollaSC[opsz,wght].woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/piazzolla/PiazzollaSC-Italic[opsz,wght].woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-piazzolla-sc",
+})
+
+export const crimson = localFont({
+  src: [
+    {
+      path: "./fonts/crimson/CrimsonPro[wght].woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/crimson/CrimsonPro-Italic[wght].woff2",
+      style: "italic",
+    },
+  ],
+  variable: "--font-crimson",
+})
+
+export const manrope = localFont({
+  src: "./fonts/manrope/Manrope[wght].woff2",
+  variable: "--font-manrope",
+})
+
+export const spaceGrotesk = localFont({
+  src: "./fonts/space-grotesk/SpaceGrotesk[wght].woff2",
+  style: "normal",
+  variable: "--font-space-grotesk",
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,120 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      <div className="text-container">
+        <div className="text">
+          <h1>Sphinx of black quartz, judge my vow</h1>
+          <p className="stand-first">
+            The pangram “The quick brown fox jumps over the lazy dog”, and the
+            search for a shorter pangram, are the cornerstone of the plot of the
+            novel Ella Minnow Pea by Mark Dunn. The search successfully comes to
+            an end when the phrase “Pack my box with five dozen liquor jugs” is
+            discovered (which has only 6 duplicated vowels).
+          </p>
+          <p>
+            The scientific paper{" "}
+            <em>
+              Cneoridium dumosum (Nuttall) Hooker F. Collected March 26, 1960,
+              at an Elevation of about 1450 Meters on Cerro Quemazón, 15 Miles
+              South of Bahía de Los Angeles, Baja California, México, Apparently
+              for a Southeastward Range Extension of Some 140 Miles
+            </em>{" "}
+            has a pangrammatic title, seemingly by pure chance. As of January
+            2022, its English Wikipedia article is the only English Wikipedia
+            article to have a pangrammatic title without having been constructed
+            as a pangram.
+          </p>
+          <p>
+            The pangram “The quick brown fox jumps over the lazy dog”, and the
+            search for a shorter pangram, are the cornerstone of the plot of the
+            novel Ella Minnow Pea by Mark Dunn. The search successfully comes to
+            an end when the phrase “Pack my box with five dozen liquor jugs” is
+            discovered (which has only 6 duplicated vowels).
+          </p>
+          <h2>Sphinx of black quartz, judge my vow</h2>
+          <p>
+            The scientific paper{" "}
+            <em>
+              Cneoridium dumosum (Nuttall) Hooker F. Collected March 26, 1960,
+              at an Elevation of about 1450 Meters on Cerro Quemazón, 15 Miles
+              South of Bahía de Los Angeles, Baja California, México, Apparently
+              for a Southeastward Range Extension of Some 140 Miles
+            </em>{" "}
+            has a pangrammatic title, seemingly by pure chance. As of January
+            2022, its English Wikipedia article is the only English Wikipedia
+            article to have a pangrammatic title without having been constructed
+            as a pangram.
+          </p>
+          <h3>Sphinx of black quartz, judge my vow</h3>
+          <p>
+            The scientific paper{" "}
+            <em>
+              Cneoridium dumosum (Nuttall) Hooker F. Collected March 26, 1960,
+              at an Elevation of about 1450 Meters on Cerro Quemazón, 15 Miles
+              South of Bahía de Los Angeles, Baja California, México, Apparently
+              for a Southeastward Range Extension of Some 140 Miles
+            </em>{" "}
+            has a pangrammatic title, seemingly by pure chance. As of January
+            2022, its English Wikipedia article is the only English Wikipedia
+            article to have a pangrammatic title without having been constructed
+            as a pangram.
+          </p>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <p>
+            The machine was successful in identifying the following ‘magic’
+            translation:
+          </p>
+          <blockquote>
+            This pangram contains four As, one B, two Cs, one D, thirty Es, six
+            Fs, five Gs, seven Hs, eleven Is, one J, one K, two Ls, two Ms,
+            eighteen Ns, fifteen Os, two Ps, one Q, five Rs, twenty-seven Ss,
+            eighteen Ts, two Us, seven Vs, eight Ws, two Xs, three Ys, & one Z.
+          </blockquote>
+          <p>
+            The pangram “The quick brown fox jumps over the lazy dog”, and the
+            search for a shorter pangram, are the cornerstone of the plot of the
+            novel Ella Minnow Pea by Mark Dunn. The search successfully comes to
+            an end when the phrase “Pack my box with five dozen liquor jugs” is
+            discovered (which has only 6 duplicated vowels).
+          </p>
+          <h1>Sphinx of black quartz, judge my vow</h1>
+          <p>
+            The pangram “The quick brown fox jumps over the lazy dog”, and the
+            search for a shorter pangram, are the cornerstone of the plot of the
+            novel Ella Minnow Pea by Mark Dunn. The search successfully comes to
+            an end when the phrase “Pack my box with five dozen liquor jugs” is
+            discovered (which has only 6 duplicated vowels).
+          </p>
+          <h2>Sphinx of black quartz, judge my vow</h2>
+          <p>
+            The pangram “The quick brown fox jumps over the lazy dog”, and the
+            search for a shorter pangram, are the cornerstone of the plot of the
+            novel Ella Minnow Pea by Mark Dunn. The search successfully comes to
+            an end when the phrase “Pack my box with five dozen liquor jugs” is
+            discovered (which has only 6 duplicated vowels).
+          </p>
+          <h3>Sphinx of black quartz, judge my vow</h3>
+          <p>
+            The pangram “The quick brown fox jumps over the lazy dog”, and the
+            search for a shorter pangram, are the cornerstone of the plot of the
+            novel Ella Minnow Pea by Mark Dunn. The search successfully comes to
+            an end when the phrase “Pack my box with five dozen liquor jugs” is
+            discovered (which has only 6 duplicated vowels).
+          </p>
+
+          <p>
+            When a word is abbreviated to more than a single letter and was
+            originally spelled with lower case letters then there is no need for
+            capitalization. However, when abbreviating a phrase where only the
+            first letter of each word is taken, then all letters should be
+            capitalized, as in <abbr>YTD</abbr> for year-to-date,{" "}
+            <abbr>PCB</abbr> for printed circuit board and <abbr>FYI</abbr> for
+            for your information.
+          </p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </main>
+  )
 }
